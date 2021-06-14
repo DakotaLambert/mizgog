494c73c4bedbea9d048d908d2c1e685afa740f95
Dakota Lambert <dakotalambertbiz@gmail.com>
2021-06-14 14:17:16 -0400

    initial page render

---
+++ src/Mizgog.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import { Route, Redirect } from "react-router-dom";
+// import { ApplicationViews } from "./ApplicationViews";
+// import { NavBar } from "./nav/NavBar";
+// import { Login } from "./auth/Login";
+// import { Register } from "./auth/Register";
+import "./Mizgog.css"
+
+export const Mizgog = () => {
+  return (
+    <div>
+      <h1 className="mizgogPageHeader">MIZGOGS M'ACCOUTREMENTS</h1>
+    </div>
+  );
+};
